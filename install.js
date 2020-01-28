98a27a6d25b088206591a11e3e2fad83b3b17bae
Christian Talmo <talmo.christian@gmail.com>
2020-01-28 23:56:53 +0200

    chore: removed old puppeteer copy/pasted files

--- install.js
+++ install.js
@@ -1,108 +1,207 @@
-/**
- * Copyright 2017 Google Inc. All rights reserved.
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *     http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
+const downloadURLs = {
+  mac: 'https://github.com/macchrome/macstable/releases/download/v79.0.3945.130-r706915-macOS/Chromium.79.0.3945.130.nosync.app.zip',
+  win32: 'https://github.com/macchrome/winchrome/releases/download/v79.0.3945.130-r706915-Win64/Ungoogled-Chromium-79.0.3945.130-Polly.Win32.7z',
+  win64: 'https://github.com/macchrome/winchrome/releases/download/v79.0.3945.130-r706915-Win64/ungoogled-chromium-79.0.3945.130-1_windows.7z'
+}
 
-const BrowserFetcher = require( './BrowserFetcher.js' )
-const browserFetcher = new BrowserFetcher( process.cwd() )
+const os = require( 'os' )
+const fs = require( 'fs' )
+const path = require( 'path' )
 
-// ref: https://chromium.woolyss.com
+const rimraf = require( 'rimraf' )
+const pkgDir = require( 'pkg-dir' )
+const extractZip = require( 'extract-zip' )
+const { unzip } = require( 'cross-unzip' )
+const makeDir = require( 'make-dir' )
+
+const https = require( 'https' )
+const http = require( 'http' )
+
+const redstar = require( 'redstar' )
+
 const revision = 706915
 
-const revisionInfo = browserFetcher.revisionInfo(revision);
+main()
 
-// Do nothing if the revision is already downloaded.
-if (revisionInfo.local) {
-  generateProtocolTypesIfNecessary(false /* updated */);
-  return;
+async function main () {
+  const projectRoot = __dirname
+  const downloadRootDirectory = path.join( projectRoot, '.local-chromium-all-codecs' )
+
+  let platform = ''
+
+  const p = os.platform()
+  if ( p === 'darwin' ) platform = 'mac'
+  else if ( p === 'linux' ) platform = 'linux'
+  else if ( p === 'win32' ) {
+    platform = os.arch() === 'x64' ? 'win64' : 'win32'
+  }
+
+  if ( !platform ) throw new Error( 'Unspported platform: ' + p )
+
+  if ( platform === 'linux' ) {
+    console.log()
+    console.log( 'See download/install instructions from here: ' )
+    console.log(
+      'https://chromium.woolyss.com/#linux'
+    )
+    return
+  }
+
+  let execName = 'chrome.exe'
+  let execPath = ''
+  let downloadDirectory = downloadRootDirectory
+  if ( platform === 'mac' ) {
+    downloadDirectory = path.join( downloadRootDirectory, 'mac-' + revision )
+    execName = 'Chromium'
+  } else if ( platform === 'linux' ) {
+    execPath = 'chromium' // global binary installed with package manager
+  } else if ( platform === 'win32' ) {
+    downloadDirectory = path.join( downloadRootDirectory, 'win32-' + revision )
+    const filename = downloadURLs[ 'win32' ].split( '/' ).pop().split( '.' )[ 0 ]
+    execName = 'chrome.exe'
+  } else if ( platform === 'win64' ) {
+    downloadDirectory = path.join( downloadRootDirectory, 'win64-' + revision )
+    const filename = downloadURLs[ 'win64' ].split( '/' ).pop().split( '.' )[ 0 ]
+    execName = 'chrome.exe'
+  } else throw new Error( 'Unspported platform: ' + platform )
+
+  // find bin path through glob pattern
+  const execPromise = new Promise(function ( resolve ) {
+    redstar( path.join( downloadDirectory, '**/' + execName ), function ( err, files, dirs ) {
+      resolve( files[ 0 ] || '' )
+    } )
+  })
+
+  execPath = await execPromise
+
+  if ( execPath ) {
+    execPath = path.relative( process.cwd(), execPath )
+  }
+
+  // make sure download directory exists
+  makeDir.sync( downloadDirectory )
+
+  try {
+    const s = fs.statSync( execPath )
+    fs.writeFileSync( path.join( __dirname, 'bin-path.txt' ), execPath, 'utf8' )
+    // exists already, no need to download
+    console.log( 'Chromium already exists, no need to download.' )
+  } catch ( err ) {
+    // doesn't exist, need to download
+    console.log( 'need to download' )
+    const url = downloadURLs[ platform ]
+
+    download( url )
+    function download ( url ) {
+      const params = require( 'url' ).parse( url )
+      // params.port = 443
+      // params.method = 'GET'
+
+      // console.log( params )
+
+      const h = ( params.protocol === 'https:' ) ? https : http
+
+      const destinationPath = path.join( downloadDirectory, `download-${ platform }-${ revision }.zip` )
+
+      let downloadedBytes = 0
+      let totalBytes = 0
+
+      console.log( 'downloading...' )
+      const req = h.request( params, function ( res ) {
+        // handle redirects
+        if ( res.statusCode >= 300 && res.statusCode < 400 && res.headers.location ) {
+          console.log( '(redirect: ' + res.statusCode + ')' )
+          return download( res.headers.location )
+        }
+
+        if ( res.statusCode !== 200 ) {
+          const error = new Error( `Download failed: server returned code ${ res.statusCode }. URL: ${ url }` )
+          // consume response data to free up memory
+          res.resume()
+          throw error
+        }
+        console.log( 'res.status: ' + res.statusCode )
+
+        totalBytes = Number( res.headers[ 'content-length' ] )
+
+        const file = fs.createWriteStream( destinationPath )
+        file.on( 'finish', onFinish )
+        file.on( 'error', onError )
+        res.pipe( file )
+
+        res.on( 'data', onData )
+      } )
+
+      req.on( 'error', onError )
+      req.end()
+
+      function onData ( chunk ) {
+        downloadedBytes += chunk.length
+        onProgress( downloadedBytes, totalBytes )
+      }
+
+      function onFinish () {
+        console.log( 'Download Finished! Unzipping...' )
+
+        // unzip
+        const zipPath = destinationPath
+        unzip(zipPath, downloadDirectory, async function ( err ) {
+          if ( err ) throw err
+          console.log( 'Unzip Success!' )
+
+          // find bin path through glob pattern
+          const execPromise = new Promise(function ( resolve ) {
+            redstar( path.join( downloadDirectory, '**/' + execName ), function ( err, files, dirs ) {
+              resolve( files[ 0 ] || '' )
+            } )
+          })
+
+          execPath = await execPromise
+
+          if ( execPath ) {
+            execPath = path.relative( process.cwd(), execPath )
+          }
+
+          try {
+            const s = fs.statSync( execPath )
+            fs.writeFileSync( path.join( __dirname, 'bin-path.txt' ), execPath, 'utf8' )
+
+            // exists already, no need to download
+            console.log( 'Chromium downloaded successfully!' )
+            console.log( 'Chromium: ' + execPath )
+          } catch ( err ) {
+            // should exist now that we just downloaded it...
+            throw new Error( 'Failed to download Chromium' )
+          }
+        } )
+      }
+
+      function onError ( err ) {
+        throw err
+      }
+    }
+  }
 }
 
-// Override current environment proxy settings with npm configuration, if any.
-const NPM_HTTPS_PROXY = process.env.npm_config_https_proxy || process.env.npm_config_proxy;
-const NPM_HTTP_PROXY = process.env.npm_config_http_proxy || process.env.npm_config_proxy;
-const NPM_NO_PROXY = process.env.npm_config_no_proxy;
-
-if (NPM_HTTPS_PROXY)
-  process.env.HTTPS_PROXY = NPM_HTTPS_PROXY;
-if (NPM_HTTP_PROXY)
-  process.env.HTTP_PROXY = NPM_HTTP_PROXY;
-if (NPM_NO_PROXY)
-  process.env.NO_PROXY = NPM_NO_PROXY;
-
-browserFetcher.download(revisionInfo.revision, onProgress)
-    .then(() => browserFetcher.localRevisions())
-    .then(onSuccess)
-    .catch(onError);
-
-/**
- * @param {!Array<string>}
- * @return {!Promise}
- */
-function onSuccess(localRevisions) {
-  logPolitely('Chromium downloaded to ' + revisionInfo.folderPath);
-  localRevisions = localRevisions.filter(revision => revision !== revisionInfo.revision);
-  // Remove previous chromium revisions.
-  const cleanupOldVersions = localRevisions.map(revision => browserFetcher.remove(revision));
-  return Promise.all([...cleanupOldVersions, generateProtocolTypesIfNecessary(true /* updated */)]);
-}
-
-/**
- * @param {!Error} error
- */
-function onError(error) {
-  console.error(`ERROR: Failed to download Chromium r${revision}! Set "PUPPETEER_SKIP_CHROMIUM_DOWNLOAD" env variable to skip download.`);
-  console.error(error);
-  process.exit(1);
-}
-
-let progressBar = null;
-let lastDownloadedBytes = 0;
-function onProgress(downloadedBytes, totalBytes) {
-  if (!progressBar) {
-    const ProgressBar = require('progress');
-    progressBar = new ProgressBar(`Downloading Chromium r${revision} - ${toMegabytes(totalBytes)} [:bar] :percent :etas `, {
+let progressBar = null
+let lastDownloadedBytes = 0
+function onProgress ( downloadedBytes, totalBytes ) {
+  if ( !progressBar ) {
+    // initialize progress bar if it doesn't exist
+    const ProgressBar = require( 'progress' )
+    progressBar = new ProgressBar( `Downloading Chromium r${ revision } - ${ toMegabytes( totalBytes ) } [:bar] :percent :etas `, {
       complete: '=',
       incomplete: ' ',
       width: 20,
-      total: totalBytes,
-    });
+      total: totalBytes
+    } )
   }
-  const delta = downloadedBytes - lastDownloadedBytes;
-  lastDownloadedBytes = downloadedBytes;
-  progressBar.tick(delta);
+  const delta = downloadedBytes - lastDownloadedBytes
+  lastDownloadedBytes = downloadedBytes
+  progressBar.tick( delta )
 }
 
-function toMegabytes(bytes) {
-  const mb = bytes / 1024 / 1024;
-  return `${Math.round(mb * 10) / 10} Mb`;
+function toMegabytes ( bytes ) {
+  const mb = bytes / 1024 / 1024
+  return `${Math.round( mb * 10 ) / 10} Mb`
 }
-
-function generateProtocolTypesIfNecessary(updated) {
-  const fs = require('fs');
-  const path = require('path');
-  if (!fs.existsSync(path.join(__dirname, 'utils', 'protocol-types-generator')))
-    return;
-  if (!updated && fs.existsSync(path.join(__dirname, 'lib', 'protocol.d.ts')))
-    return;
-  return require('./utils/protocol-types-generator');
-}
-
-function logPolitely(toBeLogged) {
-  const logLevel = process.env.npm_config_loglevel;
-  const logLevelDisplay = ['silent', 'error', 'warn'].indexOf(logLevel) > -1;
-
-  if (!logLevelDisplay)
-    console.log(toBeLogged);
-}
-
