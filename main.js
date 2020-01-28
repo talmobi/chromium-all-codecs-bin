e4f2d6793e5168db9fbef2ff723678957aa1d1ac
Christian Talmo <talmo.christian@gmail.com>
2020-01-29 00:20:48 +0200

    feat: return relative paths, attach absolute path

--- main.js
+++ main.js
@@ -2,4 +2,7 @@
 const fs = require( 'fs' )
 const path = require( 'path' )
 const binPath = fs.readFileSync( path.join( __dirname, 'bin-path.txt' ), 'utf8' )
-module.exports = path.resolve( binPath )
+module.exports = function () {
+  return path.relative( process.cwd(), binPath )
+}
+module.exports.absolute = binPath
