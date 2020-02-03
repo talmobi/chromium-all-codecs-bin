4eaa08cf5637b81f244c4f57a7cc18466f2ea433
Christian Talmo <talmo.christian@gmail.com>
2020-02-03 23:02:19 +0200

    feat: fallback for user locale chromium if available in linux

--- main.js
+++ main.js
@@ -1,8 +1,29 @@
 // give bin path to downloaded Chromium
 const fs = require( 'fs' )
 const path = require( 'path' )
+const commandExists = require( 'command-exists' )
+
+let platform = require( './get-platform.js' )
+
 const binPath = fs.readFileSync( path.join( __dirname, 'bin-path.txt' ), 'utf8' )
 module.exports = function () {
+  if ( platform === 'linux' ) {
+    // fallback to user locale stuff if available as the
+    // prebuilt binary downloaded for linux is not an
+    // all-codec version but a default chromium binary
+    if ( commandExists.sync( 'chromium-browser' ) ) {
+      return 'chromium-browser'
+    }
+
+    if ( commandExists.sync( 'chromium' ) ) {
+      return 'chromium'
+    }
+
+    if ( commandExists.sync( 'chrome' ) ) {
+      return 'chrome'
+    }
+  }
+
   return path.relative( process.cwd(), binPath )
 }
 module.exports.absolute = binPath
